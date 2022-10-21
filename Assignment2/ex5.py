dccc292b9020f284ab1c5e3f9b7c919855a8bddf
Felix_Desktop <dede6487@gmail.com>
2022-10-21 23:03:28 +0200

    finishing ass2

--- Assignment2/ex5.py
+++ Assignment2/ex5.py
@@ -1,6 +1,6 @@
-actions = "Available actions: \n a - Add numbers \n v - View statistics \n x - Exit the program \n"
+actions = "\nAvailable actions: \n a - Add numbers \n v - View statistics \n x - Exit the program \n"
 
-print("Welcome to Data Statistics! \n \n" + actions)
+print("Welcome to Data Statistics! \n")
 
 numbers = []
 count = 0
@@ -9,25 +9,34 @@ avg = 0
 min_ = 0
 max_ = 0
 
-
-inp1 = None
-while inp1 != "x":
+while True:
+    print(actions)
     inp1 = input("Enter action: ")
-    if inp1 == "a"
-        inp2 = None
-        while inp2 != "x":
+    if inp1 == "a":
+        while True:
             inp2 = input("Enter number or 'x' when you are done: ")
-            numbers += [inp2]
-            count += 1
-            sum_ += inp2
-            avg = sum_ / count
-            for n in numbers:
-
+            if inp2 != "x":
+                numbers += [int(inp2)]
+                count += 1
+                sum_ += int(inp2)
+                avg = sum_ / count
+                if int(inp2) < min_ or count == 1:
+                    min_ = int(inp2)
+                elif int(inp2) > max_ or count  == 1:
+                    max_ = int(inp2)
+            else:
+                break
     elif inp1 == "v":
         if count == 0:
             print("No numbers have been added yet!")
         else:
             print("Count: " + str(count))
-
+            print("Sum: " + str(sum_))
+            print("Avg: " + str(avg))
+            print("Min: " + str(min_))
+            print("Max: " + str(max_))
+    elif inp1 == "x":
+        print("Bye!")
+        break
     else:
         print("Invalid action '" + str(inp1) + "'. Try again!")
