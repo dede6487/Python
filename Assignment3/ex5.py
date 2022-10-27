7b2557914b5c849bbd90d413e3c6ce678070d800
Felix_Desktop <dede6487@gmail.com>
2022-10-27 13:51:49 +0200

    ass3 ex5

--- Assignment3/ex5.py
+++ Assignment3/ex5.py
@@ -1,1 +1,27 @@
-actions = "Available actions: \na - Add lifter \nr - Remove lifter"
+import copy
+
+actions = "\nAvailable actions: \na - Add lifter \nr - Remove lifter \nu - Update lifter\nv - View lifters \nx - Exit the program"
+
+lifters = dict()
+
+emptyEntry = {"squat": [], "bench": [], "dead": []}
+
+print("Welcome to Powerlifting Data Collector!\n")
+
+while True:
+    print(actions)
+    inp = input("Enter action: ")
+    if inp == "a":
+        name = input("Enter new lifter name: ")
+        if name not in lifters.keys():
+            lifters[name] = copy.deepcopy(emptyEntry)
+        else:
+            print("Lifter '" + name + "' already exists!")
+    elif inp == "r":
+        name = input("Enter lifter name to remove: ")
+        lifters.pop(name, print("Lifter '" + name + "' does not exist!"))
+    elif inp == "u":
+        name = input("Enter lifter name to update: ")
+
+    else:
+        print("Invalid action '" + inp + "'. Try again!")
