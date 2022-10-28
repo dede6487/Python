8ab1cd59fe360c07f6dfe6e1d23709fe0a639b81
Felix_Desktop <dede6487@gmail.com>
2022-10-28 11:20:32 +0200

    finishing ass3

--- Assignment3/ex5.py
+++ Assignment3/ex5.py
@@ -4,7 +4,7 @@ actions = "\nAvailable actions: \na - Add lifter \nr - Remove lifter \nu - Updat
 
 lifters = dict()
 
-emptyEntry = {"squat": [], "bench": [], "dead": []}
+emptyEntry = {"squat": list(), "bench press": list(), "deadlift": list()}
 
 print("Welcome to Powerlifting Data Collector!\n")
 
@@ -19,14 +19,29 @@ while True:
             print("Lifter '" + name + "' already exists!")
     elif inp == "r":
         name = input("Enter lifter name to remove: ")
-        lifters.pop(name, print("Lifter '" + name + "' does not exist!"))
+        x = lifters.pop(name, False)
+        if not x:
+            print("Lifter '" + name + "' does not exist!")
     elif inp == "u":
         name = input("Enter lifter name to update: ")
         if name in lifters.keys():
             update = input("Enter lift (one of 'squat', 'bench press', 'deadlift'): ")
             weight = input("Enter weight(s): ")
-
+            lifters[name][update].extend(float(a) for a in weight.split())
         else:
             print("Lifter '" + name + "' does not exist!")
+    elif inp == "v":
+        for l in lifters:
+            print("------------------------------")
+            print("Name: " + l)
+            print("squat: ", end="")
+            print(lifters[l]["squat"])
+            print("bench press: ", end="")
+            print(lifters[l]["bench press"])
+            print("deadlift: ", end="")
+            print(lifters[l]["deadlift"])
+    elif inp == "x":
+        print("Bye!")
+        break
     else:
         print("Invalid action '" + inp + "'. Try again!")
