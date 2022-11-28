442dfe943ee155eb874476d31d76fa13fed5cda4
Felix_Desktop <dede6487@gmail.com>
2022-11-28 19:18:30 +0100

    ass 5 ex 3 done

--- Assignment5/ex3.py
+++ Assignment5/ex3.py
@@ -1,3 +1,17 @@
 def binary_search(elements: list, x) -> bool:
 
+    if len(elements) == 0:
+        return False
 
+    if not isinstance(x, type(elements[0])):
+        return False
+
+    m = int(len(elements)/2)
+    if len(elements) <= 0:
+        return False
+    if elements[m] == x:
+        return True
+    elif elements[m] > x:
+        return binary_search(elements[:m-1], x)
+    elif elements[m] < x:
+        return binary_search(elements[m+1:], x)
