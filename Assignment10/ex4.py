6899a96b7a1906f409b20f0e056979c3b383a68e
Felix_Desktop <dede6487@gmail.com>
2023-01-22 18:26:23 +0100

    ass10 done

--- Assignment10/ex4.py
+++ Assignment10/ex4.py
@@ -3,14 +3,14 @@ import random
 
 
 def create_minefield(rows: int, cols: int, n_mines: int, seed=None) -> np.ndarray:
-    if n_mines <= 1 or n_mines >= rows*cols:
+    if n_mines < 1 or n_mines >= rows*cols or rows < 2 or cols < 2:
         raise ValueError
 
     # determine where to put the "mines"
     mines = np.zeros(n_mines, dtype=int)
     j = 0
     while j < n_mines:
-        temp = random.randint(0, rows*cols)
+        temp = random.randint(0, rows*cols - 1)
         if temp not in mines:
             mines[j] = temp
             j += 1
@@ -18,10 +18,25 @@ def create_minefield(rows: int, cols: int, n_mines: int, seed=None) -> np.ndarra
     arr = np.zeros(rows*cols, dtype=int)
 
     for i in mines:
-        arr[i] = -1
+        if i % cols != 0:
+            arr[i - 1] += 1
+        if i % cols != cols - 1:
+            arr[i + 1] += 1
+        if i >= cols:
+            arr[i - cols] += 1
+            if i % cols != 0:
+                arr[i - cols - 1] += 1
+            if i % cols != cols - 1:
+                arr[i - cols + 1] += 1
+        if i < cols*(rows - 1):
+            arr[i + cols] += 1
+            if i % cols != 0:
+                arr[i + cols - 1] += 1
+        if i < cols * (rows - 1) - 1 and i % cols != cols - 1:
+            arr[i + cols + 1] += 1
+
+    for i in mines:
+        arr[i] = -1  # placing mines
 
     arr = arr.reshape((rows, cols))
     return arr
-
-
-print(create_minefield(7, 7, 3, 0))
